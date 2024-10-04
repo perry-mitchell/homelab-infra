921696face3fd71758f167568ca8bce00053d40d
Perry Mitchell <perry@perrymitchell.net>
2024-10-04 22:03:54 +0300

    Tidy up

--- modules/nomad-service/storage.tf
+++ modules/nomad-service/storage.tf
@@ -5,33 +5,6 @@ data "nomad_plugin" "container_storage" {
     wait_for_healthy = true
 }
 
-# resource "nomad_volume" "container_storage" {
-#     depends_on  = [data.nomad_plugin.container_storage]
-#     # type        = "csi"
-#     plugin_id   = data.nomad_plugin.container_storage.id
-#     volume_id   = "container_${var.name}"
-#     name        = "container_${var.name}"
-#     external_id = "${var.storage.server}:${var.storage.mount}"
-
-#     capability {
-#         access_mode     = "single-node-writer"
-#         attachment_mode = "file-system"
-#     }
-# }
-
-# resource "nomad_csi_volume_registration" "container_storage" {
-#     depends_on  = [data.nomad_plugin.container_storage]
-#     plugin_id   = data.nomad_plugin.container_storage.id
-#     volume_id   = "container_${var.name}"
-#     name        = "container_${var.name}"
-#     external_id = "${var.storage.server}:${var.storage.mount}"
-
-#     capability {
-#     access_mode     = "single-node-writer"
-#     attachment_mode = "file-system"
-#   }
-# }
-
 resource "nomad_csi_volume" "container_storage" {
     depends_on  = [data.nomad_plugin.container_storage]
     count = var.storage != null ? 1 : 0
@@ -39,7 +12,6 @@ resource "nomad_csi_volume" "container_storage" {
     plugin_id   = data.nomad_plugin.container_storage.0.id
     volume_id   = "container_${var.name}"
     name        = "container_${var.name}"
-    # external_id = "${var.storage.server}:${var.storage.mount}"
 
     capacity_min = "10GiB"
     capacity_max = "20GiB"
